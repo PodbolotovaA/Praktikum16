072364b01b662e1e0c7d4da93703f453da28d8ff
PodbolotovaA <87617971+PodbolotovaA@users.noreply.github.com>
2021-09-15 22:35:01 +0200

    Add files via upload

---
+++ PetHouse.py
@@ -0,0 +1,7 @@
+from Cats import Cat
+
+cat_1 = Cat('Брон', 'мальчик', 2)
+cat_2 = Cat('Сэм', 'мальчик', 2)
+
+print('Данные о питоце :', cat_1.pet_info())
+print('Данные о питоце :', cat_2.pet_info())
