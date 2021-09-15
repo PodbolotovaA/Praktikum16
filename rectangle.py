9de070db9d4685bd2c4dc3b8ae26e60f7a79376d
PodbolotovaA <87617971+PodbolotovaA@users.noreply.github.com>
2021-09-15 22:11:07 +0200

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,21 @@
+import math
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_Area(self):
+        return self.a * self.b
+
+class Square:
+    def __init__(self, a):
+        self.a = a
+    def get_Square_Area(self):
+        return self.a ** 2
+
+class Circle:
+    def __init__(self, a):
+        self.a = a
+
+    def get_Radius(self):
+        return math.pi * (self.a **2)
